8337c7433469566d358a34cc3b6601c360d0ecbd
fahad105 <m.f.ali1995@gmail.com>
2020-07-23 01:04:19 +0200

    finished dashboard graph

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -3,7 +3,8 @@ import './Dashboard.css';
 import 'azure-storage';
 import azurestorage from 'azure-storage';
 import keys from '../keys/keys.json'; //todo: this is not secure and should be changed for sure lmao, getSASToken in Azure Function, httpTrigger
-
+import { XYPlot, XAxis, YAxis, LineSeries, LineMarkSeries, HorizontalGridLines, VerticalGridLines } from 'react-vis';
+import '../../node_modules/react-vis/dist/style.css';
 
 function Entry(props) {
     let weight = props.entry.Weight ? props.entry.Weight._ : "No Weight value detected";
@@ -12,17 +13,49 @@ function Entry(props) {
     )
 }
 
+//probably extract this to a separate class component?
 function EntryList(props) {
     if (!props.entries) {
         return (<div>Loading...</div>)
     }
     else {
+        const data = props.entries.map((entry) => {
+            return {
+                x: new Date(entry.Date._),
+                y: entry.Weight ? entry.Weight._ : null
+            }
+        });
+
         return (
-            <ul>
-                {props.entries.map((entry) => {
-                    return <Entry key={entry.RowKey._} entry={entry}></Entry>
-                })}
-            </ul>
+            <div>
+                <ul>
+                    {props.entries.map((entry) => {
+                        return <Entry key={entry.RowKey._} entry={entry}></Entry>
+                    })}
+                </ul>
+
+                <XYPlot
+                    xType="time"
+                    width={600}
+                    height={350}
+                    className="xy-plot"
+                    yDomain={[87, 93]}
+                >
+                    <XAxis tickLabelAngle={0} tickFormat={v => `${v.getDate() + "/" + (v.getMonth() + 1)}`} />
+                    <YAxis />
+
+                    <VerticalGridLines />
+                    <HorizontalGridLines />
+
+                    <LineMarkSeries
+                        strokeWidth={2}
+                        data={data}
+                        curve={'curveMonotoneX'}
+                        fill={1}
+                    />
+                </XYPlot>
+            </div>
+
         )
     }
 }
@@ -37,9 +70,8 @@ class Dashboard extends React.Component {
         let query = new azurestorage.TableQuery(); //retrieves all
         await this.tableService.queryEntities('WTData', query, null, (error, result, response) => {
             if (!error) {
-                console.log(result);
                 let sortedArray = result.entries.sort(function (a, b) {
-                    return new Date(b.Date._) - new Date(a.Date._);
+                    return new Date(a.Date._) - new Date(b.Date._);
                 });
                 this.setState({ entries: sortedArray });
             }
